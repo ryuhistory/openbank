ca82e11ca43c45dddae9c9307f4b5d63c91930f1
jehyeong <jehyeong@finda.co.kr>
2022-05-21 11:48:53 +0900

    Initial commit

---
+++ src/main/java/kr/co/kcd/openbank/OpenbankApplication.java
@@ -0,0 +1,13 @@
+package kr.co.kcd.openbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/kcd/openbank/OpenbankApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.kcd.openbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
